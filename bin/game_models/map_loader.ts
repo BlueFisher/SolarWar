98c85b089552dfe82dae891f82560a822853a773
BlueFisher <blue_fisher@qq.com>
2016-09-20 09:20:12 +0800

    初步搭建地图加载框架

---
+++ bin/game_models/map_loader.ts
@@ -0,0 +1,125 @@
+export enum PlanetType {
+	None = 0,
+	Occupied
+}
+export interface MapPlanet {
+	type: PlanetType,
+	position: {
+		x: number,
+		y: number
+	},
+	size: number
+}
+
+export class MapLoader {
+	static getMap(): MapPlanet[] {
+		return [
+			{
+				type: PlanetType.None,
+				position: {
+					x: 450,
+					y: 450
+				},
+				size: 100
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 450,
+					y: 275
+				},
+				size: 50
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 275,
+					y: 450
+				},
+				size: 50
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 625,
+					y: 450
+				},
+				size: 50
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 450,
+					y: 625
+				},
+				size: 50
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 275,
+					y: 275
+				},
+				size: 25
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 625,
+					y: 275
+				},
+				size: 25
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 275,
+					y: 625
+				},
+				size: 25
+			}, {
+				type: PlanetType.None,
+				position: {
+					x: 625,
+					y: 625
+				},
+				size: 25
+			}, {
+				type: PlanetType.Occupied,
+				position: {
+					x: 150,
+					y: 275
+				},
+				size: 50
+			}, {
+				type: PlanetType.Occupied,
+				position: {
+					x: 275,
+					y: 150
+				},
+				size: 50
+			}, {
+				type: PlanetType.Occupied,
+				position: {
+					x: 150,
+					y: 450
+				},
+				size: 50
+			}, {
+				type: PlanetType.Occupied,
+				position: {
+					x: 450,
+					y: 150
+				},
+				size: 50
+			}, {
+				type: PlanetType.Occupied,
+				position: {
+					x: 150,
+					y: 625
+				},
+				size: 50
+			}, {
+				type: PlanetType.Occupied,
+				position: {
+					x: 625,
+					y: 150
+				},
+				size: 50
+			}
+		];
+	}
+}

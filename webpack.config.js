91ab659bb8ed0c755fd34f15b1df9f89c97b239b
BlueFisher <blue_fisher@qq.com>
2016-10-03 09:29:11 +0800

    删除webpack.config.ts

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: ['./bin/web_scripts/main.js'],
+    output: {
+        path: 'public/scripts',
+        filename: 'bundle.js'
+    },
+    module: {
+        preLoaders: [
+            { test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+    externals: {
+        jquery: 'window.$'
+    },
+};
